2edcc0fd74caac8b57298c5866aea3bc3d9685a9
Bratipah <bratipahmh@gmail.com>
2023-05-21 17:28:02 +0300

    update ui

--- server/frontend/src/components/pages/MenteeStories/MentorSpotlight.jsx
+++ server/frontend/src/components/pages/MenteeStories/MentorSpotlight.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 import './bb.css'
 import Spotlight from "./Spotlight";
-import mentee from "../../../assets/Images/mentee1";
-import mentee1 from "../../../assets/Images/mentee";
-import mentee2 from "../../../assets/Images/mentee2";
-import mentee3 from "../../../assets/Images/mentee5";
+import mentee from "../../../assets/Images/mentee.jpeg";
+// import sampImg from "../../../assets/Images/mentee1.jpeg";
+import menteeOne from "../../../assets/Images/mentee1.jpeg";
+import menteeTwo from "../../../assets/Images/mentee2.jpeg";
+import menteeThree from "../../../assets/Images/mentee5.jpeg";
 import FlipCard from "./FlipCard";
 
 const MentorSpotlight = () => {
@@ -13,29 +14,28 @@ const MentorSpotlight = () => {
     <div>
       {/* <Header /> */}
       <div className="mentor">
-        <h1>Mentee Stories</h1>
+        <h1>Mentees' Stories</h1>
 
         <div className="container">
           <Carousel>
             <Spotlight
               name="kingsley"
-              location="usiu"
+              location="USIU university"
               text="My mentor, Kennedy Pendo, has been an invaluable guide in my software engineering journey. His vast knowledge, experience, and passion for the field have greatly influenced my growth as a developer. Kennedy's mentorship has helped me navigate through challenges, expand my technical skills, and develop a deeper understanding of software engineering principles. His patience, willingness to answer my questions, and constructive feedback have been instrumental in my learning process. I am truly grateful for Kennedy's support and guidance, and I highly recommend him as a mentor to anyone pursuing a career in software engineering."
               image={mentee}
             />
-
-            <Spotlight
+            <Spotlight 
               name="Onesmus"
               location="Kenyatta university"
               text="My mentor, Kennedy Pendo, has been instrumental in my journey as a software engineering student. His deep knowledge and expertise in the field have greatly enhanced my learning experience. Kennedy has consistently provided valuable guidance, insightful feedback, and practical advice. He goes above and beyond to ensure I understand complex concepts and encourages me to explore innovative solutions. I am grateful for his dedication and mentorship, and I highly recommend Kennedy to anyone seeking guidance in software engineering."
-              image={mentee1}
+              image={menteeOne}
             />
           </Carousel>
         </div>
       </div>
       {/* the 3d flip cards */}
       <FlipCard
-        image={mentee1}
+        image={menteeTwo}
         title="my title"
         role="senior frontend engineer"
       />
@@ -47,14 +47,14 @@ const MentorSpotlight = () => {
               name="Mark "
               location="Multimedia univesity"
               text="My mentor, Joyce Njimu, has had a profound impact on my cybersecurity journey. Her expertise, dedication, and guidance have been instrumental in my professional growth. Joyce's deep knowledge of the field and her ability to explain complex concepts in a clear and concise manner have greatly enhanced my understanding of cybersecurity principles. She has provided me with valuable insights, practical advice, and real-world examples that have helped me navigate the challenges of the industry. Joyce's mentorship has not only improved my technical skills but also instilled in me a sense of confidence and determination. I am truly grateful for her unwavering support and I highly recommend Joyce as a mentor to anyone aspiring to excel in the field of cybersecurity."
-              image={mentee2}
+              image={mentee}
             />
 
             <Spotlight
               name="john Mabangu"
               location="UON"
               text=" Isaac has been an exceptional project management mentor. His expertise and guidance have significantly contributed to my professional growth. He simplifies complex concepts, offers timely feedback, and creates a supportive learning environment. I highly recommend Isaac as a mentor for anyone seeking project management guidance."
-              image={mentee3}
+              image={menteeThree}
             />
           </Carousel>
         </div>
--- server/frontend/src/components/pages/MentorSpotlight/MentorSpotlight.jsx
+++ server/frontend/src/components/pages/MentorSpotlight/MentorSpotlight.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 import './bb.css'
 import Spotlight from "./Spotlight";
-import mentor1 from "../../../assets/Images/mentor1";
-import mentor2 from "../../../assets/Images/mentor2";
-import mentor3 from "../../../assets/Images/mentee4";
+import mentor1 from "../../../assets/Images/mentor1.jpeg";
+import mentor2 from "../../../assets/Images/mentor2.jpeg";
+import mentor3 from "../../../assets/Images/img2.jpg";
 import FlipCard from "./FlipCard";
 
 const MentorSpotlight = () => {
@@ -12,78 +12,55 @@ const MentorSpotlight = () => {
     <div>
       {/* <Header /> */}
       <div className="mentor">
-        <h1>mentor spotlight</h1>
+        <h1>Mentors' spotlight</h1>
 
         <div className="container">
           <Carousel>
           <Spotlight
-              name="james neema"
-              location="zumi africa"
-              text="Hudson Sitati, a dedicated and motivated student, has shown great potential as a mentee in the field of digital marketing. Throughout our mentorship journey, I have witnessed Hudson's eagerness to learn, his strong work ethic, and his ability to apply new concepts effectively. He actively seeks feedback and consistently demonstrates a growth mindset. Hudson's passion for digital marketing, coupled with his determination to excel, makes him a promising mentee. I have no doubt that with continued guidance and support, Hudson Sitati will achieve remarkable success in his digital marketing career."
-              image={mentor1}
+              name="Neema James"
+              location="Kalimax africa"
+              text="Hudson Sitati, a dedicated and motivated student, has shown great potential as a mentee in the field of digital marketing. Throughout our mentorship journey, I have witnessed Hudson's eagerness to learn his ability to apply new concepts effectively. He actively seeks feedback and consistently demonstrates a growth mindset. Hudson's passion for digital marketing, coupled with his determination to excel, makes him a promising mentee. I have no doubt that with continued guidance and support, Hudson Sitati will achieve remarkable success in his digital marketing career."
+              image={mentor3}
             />
-
             <Spotlight
               name="Kennedy Pendo"
               location="alenx TechSolutions"
               text="Sydney Baraka, my mentee in software engineering, has been an absolute pleasure to work with. Their enthusiasm for learning and commitment to their craft are truly commendable. Sydney consistently demonstrates a solid grasp of software engineering concepts and applies them effectively in their projects. I am impressed by their ability to tackle complex problems and their proactive approach in seeking guidance and feedback. With Sydney's dedication and skills, I am confident they will excel in their software engineering career. It is a privilege to be their mentor and witness their growth and progress in the field."
               image={mentor2}
             />
-            <Spotlight
-              name="john mangu "
-              location="twern Limited"
-              text="Mabangu has been an exceptional mentee in project management. He possesses strong organizational skills, attention to detail, and effective leadership abilities. Jonn is proactive, and adaptable, and continuously seeks to learn and implement new methodologies. His excellent communication and collaboration skills contribute to successful project outcomes. I have no doubt that Jonn will continue to excel in his project management career and make valuable contributions to the field. It has been a privilege to mentor him, and I am excited to witness his ongoing growth and success"
-              image={mentor4}
-            />
           </Carousel>
         </div>
       </div>
       {/* the 3d flip cards */}
       <FlipCard
-        image={mentor1}
+        image={mentor2}
         title="my title"
         role="senior frontend engineer"
-      />
+      />   
+        <div className="mentor">
+      <div className="container">
+      <Carousel>
+      <Spotlight
+          name="james neema"
+          location="zumi africa"
+          text="Hudson Sitati, a dedicated and motivated student, has shown great potential as a mentee in the field of digital marketing. Throughout our mentorship journey, I have witnessed Hudson's eagerness to learn his ability to apply new concepts effectively. He actively seeks feedback and consistently demonstrates a growth mindset. Hudson's passion for digital marketing, coupled with his determination to excel, makes him a promising mentee. I have no doubt that with continued guidance and support, Hudson Sitati will achieve remarkable success in his digital marketing career."
+          image={mentor1}
+        />
+        <Spotlight
+          name="Kennedy Pendo"
+          location="alenx TechSolutions"
+          text="Sydney Baraka, my mentee in software engineering, has been an absolute pleasure to work with. Their enthusiasm for learning and commitment to their craft are truly commendable. Sydney consistently demonstrates a solid grasp of software engineering concepts and applies them effectively in their projects. I am impressed by their ability to tackle complex problems and their proactive approach in seeking guidance and feedback. With Sydney's dedication and skills, I am confident they will excel in their software engineering career. It is a privilege to be their mentor and witness their growth and progress in the field."
+          image={mentor2}
+        />
+      </Carousel>
+    </div>
+  </div>
 
-      <div className="mentor">
-        <div className="container">
-          <Carousel>
-            <Spotlight
-              name="chike agnes"
-              location="zumi africa"
-              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              image={sampImg}
-            />
 
-            <Spotlight
-              name="chike agnes"
-              location="zumi africa"
-              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              image={sampImg3}
-            />
-            <Spotlight
-              name="chike agnes"
-              location="zumi africa"
-              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              image={sampImg4}
-            />
 
-            <Spotlight
-              name="chike agnes"
-              location="zumi africa"
-              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              image={sampImg5}
-            />
-          </Carousel>
-        </div>
-      </div>
+  
 
       {/* the 3d flip cards */}
-      <FlipCard
-        image={sampImg2}
-        title="my title"
-        role="senior frontend engineer"
-      />
     </div>
   );
 };
